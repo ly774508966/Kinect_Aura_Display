0ae2987cb79415df96017ed2adf658d582c46f55
drmbt <drmbt@gmail.com>
2016-12-15 16:26:37 -0800

    initial commit

---
+++ Assets/Scripts/assignParticleColor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assignParticleColor : MonoBehaviour {
+	public ParticleSystem childParticle;
+
+
+	public void assignColor(Color color){
+		childParticle.startColor = color;
+	}
+}
---
+++ Assets/Scripts/auraDisplay.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Kinect = Windows.Kinect;
+
+public class auraDisplay : MonoBehaviour 
+{
+    public Material BoneMaterial;
+    public GameObject BodySourceManager;
+    public GameObject jointPrefab;
+    public GameObject parentGameObject;
+
+    private Dictionary<ulong, GameObject> _Bodies = new Dictionary<ulong, GameObject>();
+    private Dictionary<ulong, Color> _Colors = new Dictionary<ulong, Color>();
+    private Dictionary<ulong, float> _offColors = new Dictionary<ulong, float>();
+
+    float randomFreq = .3f;
+
+    private BodySourceManager _BodyManager;
+  
+
+
+    private Dictionary<Kinect.JointType, Kinect.JointType> _BoneMap = new Dictionary<Kinect.JointType, Kinect.JointType>()
+    {
+        { Kinect.JointType.FootLeft, Kinect.JointType.AnkleLeft },
+        { Kinect.JointType.AnkleLeft, Kinect.JointType.KneeLeft },
+        { Kinect.JointType.KneeLeft, Kinect.JointType.HipLeft },
+        { Kinect.JointType.HipLeft, Kinect.JointType.SpineBase },
+        
+        { Kinect.JointType.FootRight, Kinect.JointType.AnkleRight },
+        { Kinect.JointType.AnkleRight, Kinect.JointType.KneeRight },
+        { Kinect.JointType.KneeRight, Kinect.JointType.HipRight },
+        { Kinect.JointType.HipRight, Kinect.JointType.SpineBase },
+        
+        { Kinect.JointType.HandTipLeft, Kinect.JointType.HandLeft },
+        { Kinect.JointType.ThumbLeft, Kinect.JointType.HandLeft },
+        { Kinect.JointType.HandLeft, Kinect.JointType.WristLeft },
+        { Kinect.JointType.WristLeft, Kinect.JointType.ElbowLeft },
+        { Kinect.JointType.ElbowLeft, Kinect.JointType.ShoulderLeft },
+        { Kinect.JointType.ShoulderLeft, Kinect.JointType.SpineShoulder },
+        
+        { Kinect.JointType.HandTipRight, Kinect.JointType.HandRight },
+        { Kinect.JointType.ThumbRight, Kinect.JointType.HandRight },
+        { Kinect.JointType.HandRight, Kinect.JointType.WristRight },
+        { Kinect.JointType.WristRight, Kinect.JointType.ElbowRight },
+        { Kinect.JointType.ElbowRight, Kinect.JointType.ShoulderRight },
+        { Kinect.JointType.ShoulderRight, Kinect.JointType.SpineShoulder },
+        
+        { Kinect.JointType.SpineBase, Kinect.JointType.SpineMid },
+        { Kinect.JointType.SpineMid, Kinect.JointType.SpineShoulder },
+        { Kinect.JointType.SpineShoulder, Kinect.JointType.Neck },
+        { Kinect.JointType.Neck, Kinect.JointType.Head },
+    };
+    
+    void Update () 
+    {
+        if (BodySourceManager == null)
+        {
+            return;
+        }
+        
+        _BodyManager = BodySourceManager.GetComponent<BodySourceManager>();
+        if (_BodyManager == null)
+        {
+            return;
+        }
+        
+        Kinect.Body[] data = _BodyManager.GetData();
+        if (data == null)
+        {
+            return;
+        }
+        
+        List<ulong> trackedIds = new List<ulong>();
+        foreach(var body in data)
+        {
+            if (body == null)
+            {
+                continue;
+              }
+                
+            if(body.IsTracked)
+            {
+                trackedIds.Add (body.TrackingId);
+            }
+        }
+        
+        List<ulong> knownIds = new List<ulong>(_Bodies.Keys);
+        
+        // First delete untracked bodies
+        foreach(ulong trackingId in knownIds)
+        {
+            if(!trackedIds.Contains(trackingId))
+            {
+                Destroy(_Bodies[trackingId]);
+                _Bodies.Remove(trackingId);
+            }
+        }
+
+        foreach(var body in data)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            
+            if(body.IsTracked)
+            {
+                if(!_Bodies.ContainsKey(body.TrackingId))
+                {
+                    _Bodies[body.TrackingId] = CreateBodyObject(body.TrackingId);
+                    _Colors[body.TrackingId] = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+                    _offColors[body.TrackingId] = Random.Range(.02f, .2f);
+                }
+                
+                RefreshBodyObject(body, _Bodies[body.TrackingId]);
+            }
+        }
+    }
+    
+    private GameObject CreateBodyObject(ulong id)
+    {
+        GameObject body = new GameObject("Body:" + id);
+        
+        for (Kinect.JointType jt = Kinect.JointType.SpineBase; jt <= Kinect.JointType.ThumbRight; jt++)
+        {
+            //GameObject jointObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            
+            GameObject jointObj = Instantiate(jointPrefab) as GameObject;
+            jointObj.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            jointObj.name = jt.ToString();
+            jointObj.transform.parent = body.transform;
+        }
+        
+		body.transform.position = parentGameObject.transform.position;
+		body.transform.localRotation = parentGameObject.transform.localRotation;
+		body.transform.localScale = parentGameObject.transform.localScale;
+		body.transform.parent = parentGameObject.transform;
+
+		return body;
+    }
+    
+    private void assignRandomColor(GameObject jointObj){
+        Color randomColor = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
+        assignColor(jointObj, randomColor);
+    }
+
+    private void assignSomeRandom(GameObject jointObj, float freq){
+        if(Random.value < freq){
+            assignRandomColor(jointObj);
+        }
+    }
+
+    private void assignColor(GameObject jointObj, Color color){
+        assignParticleColor scpt = jointObj.GetComponent<assignParticleColor>();
+        if(scpt != null){
+            scpt.assignColor(color);
+        }      
+    }
+
+    private void RefreshBodyObject(Kinect.Body body, GameObject bodyObject)
+    {
+        for (Kinect.JointType jt = Kinect.JointType.SpineBase; jt <= Kinect.JointType.ThumbRight; jt++)
+        {
+            Kinect.Joint sourceJoint = body.Joints[jt];
+            Kinect.Joint? targetJoint = null;
+            
+            if(_BoneMap.ContainsKey(jt))
+            {
+                targetJoint = body.Joints[_BoneMap[jt]];
+            }
+            
+            GameObject jointObj = bodyObject.transform.FindChild(jt.ToString()).gameObject;
+            jointObj.transform.localPosition = GetVector3FromJoint(sourceJoint);
+            assignColor(jointObj, _Colors[body.TrackingId]);
+            assignSomeRandom(jointObj, _offColors[body.TrackingId]);
+        }
+    }
+    
+    private static Color GetColorForState(Kinect.TrackingState state)
+    {
+        switch (state)
+        {
+        case Kinect.TrackingState.Tracked:
+            return Color.green;
+
+        case Kinect.TrackingState.Inferred:
+            return Color.red;
+
+        default:
+            return Color.black;
+        }
+    }
+    
+    private static Vector3 GetVector3FromJoint(Kinect.Joint joint)
+    {
+        float randomX = Random.value * .1f;
+        float randomY = Random.value * .3f;
+        float randomZ = Random.value * .1f;
+        return new Vector3(
+            joint.Position.X * 10 + randomX,
+            joint.Position.Y * 10 + randomY,
+            joint.Position.Z * 10 + randomZ
+        );
+    }
+}
